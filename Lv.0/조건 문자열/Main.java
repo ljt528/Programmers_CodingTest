79b09d67e4d6a63b27baa9f134e26fc1b620512b
ljt528 <ljt528@naver.com>
2024-03-15 17:09:12 +0900

    24/03/15 Lv.0 풀이

---
+++ Lv.0/조건 문자열/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public int main(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        if (ineq.equals("<") && eq.equals("=")) {
+            answer = (n <= m) ? 1 : 0 ;
+        } else if (ineq.equals("<") && eq.equals("!")) {
+            answer = (n < m) ? 1 : 0 ;
+        } else if (ineq.equals(">") && eq.equals("=")) {
+            answer = (n >= m) ? 1 : 0 ;
+        } else if (ineq.equals(">") && eq.equals("!")) {
+            answer = (n > m) ? 1 : 0 ;
+        }
+        return answer;
+    }
+}
