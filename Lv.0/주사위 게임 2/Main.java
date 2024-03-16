25b67193eb7d6f869d235994fb5ab2a3f59d9d64
ljt528 <ljt528@naver.com>
2024-03-17 01:09:03 +0900

    24/03/17 Lv.0 풀이

---
+++ Lv.0/주사위 게임 2/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public int main(int a, int b, int c) {
+        int answer = 0;
+        if (a == b && b == c) {
+            answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        } else if (a != b && b != c && c != a) {
+            answer = a + b + c;
+        } else {
+            answer = (a + b + c) * (a*a + b*b + c*c);
+        }
+        return answer;
+    }
+}
