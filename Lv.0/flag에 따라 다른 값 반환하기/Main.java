a1de36a1853713152dee2e1c8a4e976349cb48df
ljt528 <ljt528@naver.com>
2024-04-25 11:02:03 +0900

    24/04/25 Lv.1 풀이

--- Lv.0/flag에 따라 다른 값 반환하기/Main.java
+++ Lv.0/flag에 따라 다른 값 반환하기/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public int main(int a, int b, boolean flag) {
+        
         int answer = flag == true ? a + b : a - b;
         return answer;
+
     }
 }
