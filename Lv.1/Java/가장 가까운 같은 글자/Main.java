9e05811ab71bb708e2013c12159389ab9579b4cc
ljt528 <ljt528@naver.com>
2024-05-01 13:15:35 +0900

    24/05/01 Lv.1 풀이

---
+++ Lv.1/Java/가장 가까운 같은 글자/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public int[] main(String s) {
+        int[] answer = new int [s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j) && answer[i] == 0) {
+                    answer[i] = i - j;
+                }
+            }
+            if (answer[i] == 0) {
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
