914466d04f573de9de81f1f1368f3443d4841108
ljt528 <ljt528@naver.com>
2024-05-08 01:38:31 +0900

    24/05/08 Lv.1 풀이

---
+++ Lv.1/Java/콜라 문제/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public int main(int a, int b, int n) {
+        int answer = 0;
+
+        while(n >= a) {
+            answer += (n / a) * b;
+            n = (n / a) * b + (n % a);
+        }
+
+        return answer;
+    }
+}
