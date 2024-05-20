e12d6fdcf8b1872423c0ff8303382986bd75130e
ljt528 <ljt528@naver.com>
2024-05-20 16:23:12 +0900

    24/05/20 Lv.1 풀이

---
+++ Lv.1/Java/내적/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public int main(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+}
