320184bbd737ff2a07d99bf89eef44b9fbb872c1
ljt528 <ljt528@naver.com>
2024-06-30 21:53:21 +0900

    24/06/30 Lv.0 풀이

---
+++ Lv.0/옷가게 할인 받기/옷가게 할인 받기.sql
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(double price) {
+        double answer = 0;
+        if (price >= 100000 && price < 300000) {
+            answer = price - (price * 0.05);
+        } else if (price >= 300000 && price < 500000) {
+            answer = price - (price * 0.1);
+        } else if (price >= 500000) {
+            answer = price - (price * 0.2);
+        } else {
+            answer = price;
+        }
+        return (int) answer;
+    }
+}
