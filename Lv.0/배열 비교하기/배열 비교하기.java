14731e138e2642d0b532d7a43d06b1ec3c8563d0
ljt528 <ljt528@naver.com>
2024-07-24 00:57:18 +0900

    24/07/24 Lv.0 풀이

---
+++ Lv.0/배열 비교하기/배열 비교하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return Integer.compare(arr1.length, arr2.length);
+        }
+        return Integer.compare(Arrays.stream(arr1).sum(), Arrays.stream(arr2).sum());
+    }
+}
